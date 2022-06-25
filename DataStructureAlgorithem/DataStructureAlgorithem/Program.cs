813e28b806c16a9b844a8c3900a6f3c77c3f397d
shreegowri <gaganandh@gmail.com>
2022-06-25 08:19:36 +0530

    [shreeegowri]bubble sort

--- DataStructureAlgorithem/DataStructureAlgorithem/Program.cs
+++ DataStructureAlgorithem/DataStructureAlgorithem/Program.cs
@@ -5,24 +5,29 @@
         public static void Main(String[] args)
         {
             //uc2:BST
-            //BinaryTree binaryTree = new BinaryTree();
-            //String[] arr = { "contribute", "geeks", "ide", "practice" };
-            //String x = "ide";
-            //int result = BinaryTree.binarySearch(arr, x);
+            ////BinaryTree binaryTree = new BinaryTree();
+            ////String[] arr = { "contribute", "geeks", "ide", "practice" };
+            ////String x = "ide";
+            ////int result = BinaryTree.binarySearch(arr, x);
 
-            //if (result == -1)
-            //    Console.WriteLine("Element not present");
-            //else
-            //    Console.WriteLine("Element found at "
-            //                    + "index " + result);
+            ////if (result == -1)
+            ////    Console.WriteLine("Element not present");
+            ////else
+            ////    Console.WriteLine("Element found at "
+            ////                    + "index " + result);
             //uc3:Insertion SORT
-            String[] arr1 = { "Bridgelabsz", "I", "from", "am" };
-            int n = arr1.Length;
+            //String[] arr1 = { "Bridgelabsz", "I", "from", "am" };
+            //int n = arr1.Length;
 
-            // Function to perform sorting
-            InsertionSort.sort(arr1, n);
-            // Calling the function to print result
-            InsertionSort.printArraystring(arr1, n);
+            //// Function to perform sorting
+            //InsertionSort.sort(arr1, n);
+            //// Calling the function to print result
+            //InsertionSort.printArraystring(arr1, n);
+            //uc4:BubbleSort
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            BubbleSort.bubbleSort(arr);
+            Console.WriteLine("Sorted array");
+            BubbleSort.printArray(arr);
         }
     }
 }
