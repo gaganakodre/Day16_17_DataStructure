a8312319e3cfa1c7fce10f518041ac44fdb7f673
shreegowri <gaganandh@gmail.com>
2022-06-25 07:55:26 +0530

    [shreeegowri]BST the word from word list

---
+++ DataStructureAlgorithem/DataStructureAlgorithem/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructureAlgorithem
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            String[] arr = { "contribute", "geeks", "ide", "practice" };
+            String x = "ide";
+            int result = BinaryTree.binarySearch(arr, x);
+
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at "
+                                + "index " + result);
+        }
+    }
+}
